e5d58929c989da3962e5a171822d4d4d42e5b314
StargazyP <jdajsl0415@naver.com>
2025-12-08 17:13:15 +0000

    테스트 설정 개선: H2 인메모리 DB를 사용한 테스트 환경 구성

--- inhatc/src/test/java/kr/co/inhatc/inhatc/InhatcApplicationTests.java
+++ inhatc/src/test/java/kr/co/inhatc/inhatc/InhatcApplicationTests.java
@@ -2,12 +2,16 @@ package kr.co.inhatc.inhatc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class InhatcApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Application context 로드 테스트
+		// 데이터베이스 연결 없이도 실행 가능하도록 설정 필요
 	}
 
 }
